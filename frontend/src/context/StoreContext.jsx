6b685b6fdf43bf402cc9d67886864c174d6153ee
Pranab Bhowmik <109957935+pranabbhowmik@users.noreply.github.com>
2024-12-18 14:59:38 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:5000";
+  const url = "https://just-cheers-backend.onrender.com";
   const [token, setToken] = useState("");
 
   const [food_list, setFoodList] = useState([]);
