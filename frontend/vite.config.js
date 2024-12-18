9684eff99000d78a5f00a43bb4131246df3238bd
Pranab Bhowmik <109957935+Pranab09122002@users.noreply.github.com>
2024-12-18 15:39:04 +0530

    Fix typo in Header.css and increase chunk size warning limit in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000, // Increase the limit to 1000 KB (2 MB)
+  },
 });
